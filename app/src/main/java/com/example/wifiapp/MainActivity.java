3a7bb80f20673575f5d1a2fa80feb79e31c364db
Victor <victor_cabral_o@hotmail.com>
2024-04-13 19:12:28 -0300

    feat: implemented method to enable wifi, show if wifi is needed & make sure if wifi is on

--- app/src/main/java/com/example/wifiapp/MainActivity.java
+++ app/src/main/java/com/example/wifiapp/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.wifiapp;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.thanosfisherman.wifiutils.WifiUtils;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,11 +22,44 @@ public class MainActivity extends AppCompatActivity {
     private List<ScanResult> results;
     private WifiManager wifiManager;
     private final ArrayList<WifiNetwork> wifiIsAvaliable = new ArrayList<>();
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void makeSureWifiIsOn() {
+        if (wifiManager.isWifiEnabled()) {
+            scanWifi();
+            return;
+        }
+        enableWifi();
+    }
+
+    private void enableWifi() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            dialog = showWifiNeeded();
+            return;
+        }
+        WifiUtils.withContext(getApplicationContext()).enableWifi();
+    }
+
+    private final ActivityResultLauncher<Intent> wifiEnabling = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), results -> makeSureWifiIsOn());
+
+    private AlertDialog showWifiNeeded() {
+        closeDialog();
+        return new AlertDialog.Builder(this)
+                .setTitle(R.string.warning).setMessage(R.string.enable_wifi)
+                .setPositiveButton(R.string.ok, (dialogInterface, i) -> wifiEnabling.launch(new Intent(Settings.Panel.ACTION_WIFI)))
+                .setCancelable(false).show();
+    }
+
+    private void closeDialog() {
+        if (dialog != null) {
+            dialog.dismiss();
+        }
     }
 
     public void scanWifi() {
